29d8f08861410037397a958fa426e501a05642a2
Fabian Virguez <fabian.virguez@infracommerce.lat>
2023-09-04 08:56:48 -0500

    first commit upload proyect

---
+++ src/config/environments.js
@@ -0,0 +1,8 @@
+const configMercadoPago = {
+  access_token: 'TEST-564374957193654-082118-972f521f9c7a4bedfde4cf784e381cac-193336365',
+  public_key: 'TEST-71079196-e579-49ca-a81a-f903cdf4b223'
+};
+
+module.exports = {
+  configMercadoPago
+}
